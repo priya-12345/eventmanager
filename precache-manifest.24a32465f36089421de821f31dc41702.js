eff968a62024dc0a69c611c4f3381db7f13b39eb
Priya Gupta <Priya.Gupta@genmills.com>
2020-06-24 19:30:43 +0530

    Updates

---
+++ precache-manifest.24a32465f36089421de821f31dc41702.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97c04f483e56a6d7fc7a008fd2a136a6",
+    "url": "/eventmanager/index.html"
+  },
+  {
+    "revision": "689ae54ef94ce5368189",
+    "url": "/eventmanager/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "9d0ac801cb4259ff11fc",
+    "url": "/eventmanager/static/css/main.382a7d88.chunk.css"
+  },
+  {
+    "revision": "689ae54ef94ce5368189",
+    "url": "/eventmanager/static/js/2.88d649da.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/eventmanager/static/js/2.88d649da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9d0ac801cb4259ff11fc",
+    "url": "/eventmanager/static/js/main.4341d0a2.chunk.js"
+  },
+  {
+    "revision": "7253f601832c26160592",
+    "url": "/eventmanager/static/js/runtime-main.37f593a6.js"
+  }
+]);
